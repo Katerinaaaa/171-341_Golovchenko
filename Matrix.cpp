cf1bdc1c7d789ff2af6f56e0b65fb1ea80990d46
Katerinaaaa <33904226+Katerinaaaa@users.noreply.github.com>
2017-12-17 22:10:25 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -5,18 +5,18 @@
 
 using namespace std;
 
-Matrix::Matrix() // создание класса матриц
+Matrix::Matrix() // СЃРѕР·РґР°РЅРёРµ РєР»Р°СЃСЃР° РјР°С‚СЂРёС†
 {
-	m = 0; // количество столбцов 
-	n = 0; // количество строк 
-	data = 0; // матрица 
+	m = 0; // РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚РѕР»Р±С†РѕРІ 
+	n = 0; // РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚СЂРѕРє 
+	data = 0; // РјР°С‚СЂРёС†Р° 
 }
 
 int Matrix::get(int i, int j) 
 {
 	if ((data != 0) &&(i < n) && (j < m))
 	{
-		return data[i * m + j]; // чтение элемента на позиции [i, j] 
+		return data[i * m + j]; // С‡С‚РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РЅР° РїРѕР·РёС†РёРё [i, j] 
 	}
 	else
 	{
@@ -24,15 +24,68 @@ int Matrix::get(int i, int j)
 	}
 }
 
+
+Matrix operator + (Matrix & matrix1, 
+				Matrix & matrix2)
+{
+	Matrix result;
+	result.i = matrix1.i + matrix2.i;
+	result.j = matrix1.j + matrix2.j;
+
+	return result; 
+}
+
+
+Matrix operator - (Matrix & matrix1,
+	Matrix & matrix2)
+{
+	Matrix result;
+	result.i = matrix1.i - matrix2.i;
+	result.j = matrix1.j - matrix2.j;
+
+	return result;
+}
+
+
+Matrix operator * (Matrix & matrix1,
+	Matrix & matrix2)
+{
+	Matrix result;
+	result.i = matrix1.i * matrix2.i;
+	result.j = matrix1.j * matrix2.j;
+
+	return result;
+}
+
+Matrix operator * (Matrix & matrix1,
+	Matrix & vector1)
+{
+	Matrix result;
+	result.j = matrix1.j * vector1.j;
+
+	return result;
+}
+
+ostream & operator << (ostream & matrix1,
+	Matrix & matrix2)
+{
+	matrix1 << "Matrix: " << endl;
+	matrix1 << "\ti = " << matrix2.i << ";" << endl;
+	matrix1 << "\tj = " << matrix2.j << ";" << endl;
+
+	return matrix1;
+}
+
+
 void Matrix::set(int i, int j, int value) 
 {
 	if ((data != 0) && (i < n) && (j < m))
 	{
-		data[i * m + j] = value; // запись элемента на позицию [i, j]
+		data[i * m + j] = value; // Р·Р°РїРёСЃСЊ СЌР»РµРјРµРЅС‚Р° РЅР° РїРѕР·РёС†РёСЋ [i, j]
 	}
 }
 
-void Matrix::set_size(int new_n, int new_m) // задание размера матрицы
+void Matrix::set_size(int new_n, int new_m) // Р·Р°РґР°РЅРёРµ СЂР°Р·РјРµСЂР° РјР°С‚СЂРёС†С‹
 {
 	int *new_data = new int[new_n * new_m];
 	for (int i = 0; i < new_n; ++i)
@@ -48,17 +101,17 @@ void Matrix::set_size(int new_n, int new_m) // задание размера матрицы
 	data = new_data; 
 }
 
-int Matrix:: num_rows() // количество строк 
+int Matrix:: num_rows() // РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚СЂРѕРє 
 {
 	return n; 
 }
 
-int Matrix::num_cols() // количество столбцов 
+int Matrix::num_cols() // РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚РѕР»Р±С†РѕРІ 
 {
 	return m; 
 }
 
-void Matrix::print() // печать матрицы 
+void Matrix::print() // РїРµС‡Р°С‚СЊ РјР°С‚СЂРёС†С‹ 
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -70,7 +123,7 @@ void Matrix::print() // печать матрицы
 	}
 }
 
-void Matrix:: matrix_mult(int **matrix_result, int **matrix1, int **matrix2, int t1, int p1, int t2, int p2) //произведение матриц 
+void Matrix:: matrix_mult(int **matrix_result, int **matrix1, int **matrix2, int t1, int p1, int t2, int p2) //РїСЂРѕРёР·РІРµРґРµРЅРёРµ РјР°С‚СЂРёС† 
 {
 
 	for (int i = 0; i < t1; i++)
@@ -80,21 +133,21 @@ void Matrix:: matrix_mult(int **matrix_result, int **matrix1, int **matrix2, int
 			int s = 0;
 			for (int z = 0; z < p1; z++)
 			{
-				s = s + matrix1[i][z] * matrix2[z][j]; // формула умножения двух матриц 
+				s = s + matrix1[i][z] * matrix2[z][j]; // С„РѕСЂРјСѓР»Р° СѓРјРЅРѕР¶РµРЅРёСЏ РґРІСѓС… РјР°С‚СЂРёС† 
 			}
 			matrix_result[i][j] = s;
 		}
 	}
 }
 
-void Matrix:: matrix_matrix(int **result, int **matrix1, int **matrix2, int n1, int m1, int n2, int m2) // функция для подсчета суммы матриц 
+void Matrix:: matrix_matrix(int **result, int **matrix1, int **matrix2, int n1, int m1, int n2, int m2) // С„СѓРЅРєС†РёСЏ РґР»СЏ РїРѕРґСЃС‡РµС‚Р° СЃСѓРјРјС‹ РјР°С‚СЂРёС† 
 {
 
 	for (int i = 0; i < n1; i++)
 	{
 		for (int j = 0; j < m1; j++)
 		{
-			result[i][j] = matrix1[i][j] + matrix2[i][j]; // формула сложения матриц
+			result[i][j] = matrix1[i][j] + matrix2[i][j]; // С„РѕСЂРјСѓР»Р° СЃР»РѕР¶РµРЅРёСЏ РјР°С‚СЂРёС†
 		}
 
 	}
@@ -102,15 +155,15 @@ void Matrix:: matrix_matrix(int **result, int **matrix1, int **matrix2, int n1, 
 
 
 
-Matrix::Matrix(int nn, int mm) // вызов конструктора 
+Matrix::Matrix(int nn, int mm) // РІС‹Р·РѕРІ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР° 
 {
 	data = 0;
 	n = 0;
 	m = 0;
-	set_size(nn, mm); // создание матрицы размером 0х0 
+	set_size(nn, mm); // СЃРѕР·РґР°РЅРёРµ РјР°С‚СЂРёС†С‹ СЂР°Р·РјРµСЂРѕРј 0С…0 
 }
 
-Matrix::~Matrix() // вызов деструктора 
+Matrix::~Matrix() // РІС‹Р·РѕРІ РґРµСЃС‚СЂСѓРєС‚РѕСЂР° 
 {
 	delete[] data; 
 }
