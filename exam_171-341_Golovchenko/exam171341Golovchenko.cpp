febee0e086696fd51d001a5c3515008aa73ee1b7
Katerinaaaa <33904226+Katerinaaaa@users.noreply.github.com>
2018-01-20 12:20:51 +0300

    успешный краш-тест

--- exam_171-341_Golovchenko/exam171341Golovchenko.cpp
+++ exam_171-341_Golovchenko/exam171341Golovchenko.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "Exam_string.h"
 
+using namespace std; 
 
 int main()
 {
+	Exam_string str1; 
+
+	/*while (1)
+	{
+		for (int i = 0; i < 10000; i++)
+		{
+			str1.insert(0, 'n'); 
+			str1.add('a');
+		}
+
+		str1.print();
+
+		cout << endl; 
+
+		str1.clear(); 
+	}*/ 
+
+	str1.add('m'); 
+	str1.print();
+
+	str1.add('n'); 
+	str1.print(); 
+
+
+	getchar(); 
+
     return 0;
 }
 
