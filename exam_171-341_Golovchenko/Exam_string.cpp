17cf871ebb7821b64f9460a8fde1b9e9bf60e865
Katerinaaaa <33904226+Katerinaaaa@users.noreply.github.com>
2018-01-20 10:05:08 +0300

    создан класс строк

---
+++ exam_171-341_Golovchenko/Exam_string.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Exam_string.h"
+#include <iostream> 
+
+
+Exam_string::Exam_string() // создание класса строк 
+{
+	int a = 0; 
+	int max = 0; 
+	char *st = 0; 
+	char *new_st = 0; 
+}
+
+
+int Exam_string::
+
+Exam_string::~Exam_string()
+{
+	delete[] st; 
+}
---
+++ exam_171-341_Golovchenko/exam171341Golovchenko.cpp
@@ -0,0 +1,11 @@
+// exam171341Golovchenko.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
