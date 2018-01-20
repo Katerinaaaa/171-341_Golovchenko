e12722f48b5ba0296a00518eb26302b1ccc94d09
Katerinaaaa <33904226+Katerinaaaa@users.noreply.github.com>
2018-01-20 15:33:38 +0300

    финальный вариант

--- exam_171-341_Golovchenko/exam171341Golovchenko.cpp
+++ exam_171-341_Golovchenko/exam171341Golovchenko.cpp
@@ -11,27 +11,30 @@ int main()
 {
 	Exam_string str1; 
 
-	/*while (1)
+	while (1)
 	{
 		for (int i = 0; i < 10000; i++)
 		{
-			str1.insert(0, 'n'); 
+			
 			str1.add('a');
+
+
+			str1.print();
+
+			cout << endl;
+
+			str1.clear();
 		}
+	}
 
-		str1.print();
+	// str1.add('m'); 
+	// str1.print();
 
-		cout << endl; 
+	// str1.add('n'); 
+	// str1.print(); 
 
-		str1.clear(); 
-	}*/ 
-
-	str1.add('m'); 
-	str1.print();
-
-	str1.add('n'); 
-	str1.print(); 
-
+//	str1.cut(2, g); 
+//	str1.print(); 
 
 	getchar(); 
 
